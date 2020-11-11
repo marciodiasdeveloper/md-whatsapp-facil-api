724c8e6154a514e6c25dd52d3405f428b52fdf29
Your Name <you@example.com>
2020-11-11 14:25:12 -0200

    Fix

--- src/server.js
+++ src/server.js
@@ -1,5 +1,33 @@
 const app = require('./app');
+const SessionService = require("../service/SessionService");
 
 console.log('http server started on port 3333');
 
 app.listen(3334);
+
+process.stdin.resume();//so the program will not close instantly
+
+async function exitHandler(options, exitCode) {
+    if (options.cleanup) {
+        console.log('cleanup');
+        await Sessions.getSessions().forEach(async session => {
+            await Sessions.closeSession(session.sessionName);
+        });
+    }
+    if (exitCode || exitCode === 0) {
+        console.log(exitCode);
+    }
+
+    if (options.exit) {
+        process.exit();
+    }
+} //exitHandler 
+//do something when app is closing
+process.on('exit', exitHandler.bind(null, { cleanup: true }));
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
