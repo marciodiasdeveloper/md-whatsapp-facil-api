181dfe261d979db4b0c92b64e73aa54d2c5942f0
Márcio Dias <eu@marciodias.me>
2020-11-12 12:38:07 -0300

    Fix

--- src/services/WebhookService.js
+++ src/services/WebhookService.js
@@ -5,12 +5,19 @@ require('dotenv/config');
 module.exports = class Sessions {
 
     static async notifyApiSessionUpdate(session) {
-        const response = await axios.post(process.env.API_WEBHOOK_URL+'/session/status', session)  
-        .then(function(response){
-            console.log('Webhook enviado com sucesso!');
-        }).catch(e => console.log(e));
+
+        let url = process.env.API_WEBHOOK_URL+'/session/status';
+
+        console.log('Disparando Webhook para ',url);
         
-        return response;
+        if(process.env.API_WEBHOOK) {
+            const response = await axios.post(url, session)  
+            .then(function(response){
+                console.log('Webhook enviado com sucesso!');
+            }).catch(e => console.log(e));
+            
+            return response;
+        }
     }
 
 }
