6068dba860cebb1dc01453d69949131e70923a35
Your Name <you@example.com>
2020-11-13 14:10:14 -0200

    Fix

--- src/services/xxx/anota.js
+++ src/services/xxx/anota.js
@@ -7,7 +7,7 @@ module.exports = class FraseAleatoria {
 
         FraseAleatoria.data = FraseAleatoria.data || [];
        
-        fs.readFile('/var/www/www.api.marciodias.me/md-whatsapp-facil-api/src/services/xxx/storage/frases.txt', 'utf8', function(err, rawData) {
+        await fs.readFile('/var/www/www.api.marciodias.me/md-whatsapp-facil-api/src/services/xxx/storage/frases.txt', 'utf8', function(err, rawData) {
         // fs.readFile('/Users/marciodias/dev/stack/saas-marciodias/md-whatsapp-facil-api/src/services/xxx/storage/frases.txt', 'utf8', function(err, rawData) {
             if(err) {
                 return console.log(err);
@@ -15,11 +15,11 @@ module.exports = class FraseAleatoria {
             FraseAleatoria.data = rawData.toString().split('\n');
         });
 
-        let frase = await FraseAleatoria.data[FraseAleatoria.randomInt(0, FraseAleatoria.data.length)];
+        let frase = FraseAleatoria.data[FraseAleatoria.randomInt(0, FraseAleatoria.data.length)];
         
         console.log('frase', frase);
 
-        return await String(frase).trim();
+        return String(frase).trim();
     }
 
     static randomInt(low,high) {
