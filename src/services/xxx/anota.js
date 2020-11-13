0b5c7ca81bbddda2effc5b0a19e6d51620b2507a
Márcio Dias <eu@marciodias.me>
2020-11-13 11:56:02 -0300

    Fix

--- src/services/xxx/anota.js
+++ src/services/xxx/anota.js
@@ -5,13 +5,13 @@ module.exports = class FraseAleatoria {
 
     static async responder(sessionName) {
 
-        FraseAleatoria.data = FraseAleatoria.data || [];
+        data = data || [];
        
         fs.readFile('/var/www/www.api.marciodias.me/md-whatsapp-facil-api/src/services/xxx/storage/frases.txt', 'utf8', function(err, rawData) {
             if(err) {
                 return console.log(err);
             }
-            FraseAleatoria.data = rawData.split('\n');
+            data = rawData.split('\n');
         });
 
         return data[FraseAleatoria.randomInt(0, data.length)];
