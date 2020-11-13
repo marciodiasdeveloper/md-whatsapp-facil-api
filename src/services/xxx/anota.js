6861429ca187b8f0a8fd03cae3904ce4d56eba03
Márcio Dias <eu@marciodias.me>
2020-11-13 12:38:06 -0300

    Fix

--- src/services/xxx/anota.js
+++ src/services/xxx/anota.js
@@ -7,15 +7,15 @@ module.exports = class FraseAleatoria {
 
         FraseAleatoria.data = FraseAleatoria.data || [];
        
-        fs.readFile('/var/www/www.api.marciodias.me/md-whatsapp-facil-api/src/services/xxx/storage/frases.txt', 'utf8', function(err, rawData) {
-        // fs.readFile('/Users/marciodias/dev/stack/saas-marciodias/md-whatsapp-facil-api/src/services/xxx/storage/frases.txt', 'utf8', function(err, rawData) {
+        // fs.readFile('/var/www/www.api.marciodias.me/md-whatsapp-facil-api/src/services/xxx/storage/frases.txt', 'utf8', function(err, rawData) {
+        fs.readFile('/Users/marciodias/dev/stack/saas-marciodias/md-whatsapp-facil-api/src/services/xxx/storage/frases.txt', 'utf8', function(err, rawData) {
             if(err) {
                 return console.log(err);
             }
             FraseAleatoria.data = rawData.toString().split('\n');
         });
 
-        let frase = FraseAleatoria.data[FraseAleatoria.randomInt(0, FraseAleatoria.data.length)];
+        let frase = JSON.stringify(FraseAleatoria.data[FraseAleatoria.randomInt(0, FraseAleatoria.data.length)]);
         
         console.log('frase', frase);
 
