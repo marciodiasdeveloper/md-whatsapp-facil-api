dc96a2c639c8ed176eb7fe5981cf2732e7de92e6
marciodias <marciodias@marciodiass-iMac.local>
2020-11-17 11:42:20 -0300

    Fix

--- src/controllers/CheckPhoneController.js
+++ src/controllers/CheckPhoneController.js
@@ -3,6 +3,11 @@ const SessionService = require("../services/SessionService");
 module.exports = {
     async index(request, response, next) {
       let result = await SessionService.checkPhone(request.query.sessionName, request.query.phone);
-      response.json(result);
+      
+      if(result) {
+        response.json({ result: true, message: 'valid' });
+      } else {
+        response.json({ result: false, message: 'not_valid' });
+      }
     },
 };
