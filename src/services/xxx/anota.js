45420bb61f436fba7b4eb6f134ebe5ed59818658
Your Name <you@example.com>
2020-11-17 20:19:25 -0200

    FIx

--- src/services/xxx/anota.js
+++ src/services/xxx/anota.js
@@ -8,7 +8,6 @@ module.exports = class FraseAleatoria {
         FraseAleatoria.data = FraseAleatoria.data || [];
        
         await fs.readFile('/var/www/www.api.marciodias.me/md-whatsapp-facil-api/src/services/xxx/storage/frases.txt', 'utf8', function(err, rawData) {
-        // fs.readFile('/Users/marciodias/dev/stack/saas-marciodias/md-whatsapp-facil-api/src/services/xxx/storage/frases.txt', 'utf8', function(err, rawData) {
             if(err) {
                 return console.log(err);
             }
@@ -17,7 +16,7 @@ module.exports = class FraseAleatoria {
 
         let frase = FraseAleatoria.data[FraseAleatoria.randomInt(0, FraseAleatoria.data.length)];
         
-        if(!frase || !frase === undefined) {
+        while(frase===undefined) {
             frase = FraseAleatoria.anotacoes[FraseAleatoria.randomInt(0, FraseAleatoria.anotacoes.length)];
         }
 
