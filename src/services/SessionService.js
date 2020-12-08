19ad7780fd4480ffcd3941ad0c0c97357a08c793
Your Name <you@example.com>
2020-12-08 03:01:41 -0200

    Fix

--- src/services/SessionService.js
+++ src/services/SessionService.js
@@ -202,23 +202,23 @@ module.exports = class Sessions {
                             await client.sendFile(message.from, pathFile, 'output.mp3', message_text.toString())
                             .then((result) => {
                               console.log('Result: ', result); //return object success
-                              fs.unlink(pathFile, (err) => {
-                                    if (err) {
-                                        console.error(err)
-                                        return
-                                    }
-                                    //file removed
-                                });
+                            //   fs.unlink(pathFile, (err) => {
+                            //         if (err) {
+                            //             console.error(err)
+                            //             return
+                            //         }
+                            //         //file removed
+                            //     });
                             })
                             .catch((erro) => {
                               console.error('Error when sending: ', erro); //return object error
-                              fs.unlink(pathFile, (err) => {
-                                    if (err) {
-                                        console.error(err)
-                                        return
-                                    }
-                                    //file removed
-                                });
+                            //   fs.unlink(pathFile, (err) => {
+                            //         if (err) {
+                            //             console.error(err)
+                            //             return
+                            //         }
+                            //         //file removed
+                            //     });
                             });
                         } else {
                             let message_text = '*'+message.sender.pushname+'*, '+msg.toString();
@@ -287,24 +287,24 @@ module.exports = class Sessions {
                         .then((result) => {
                           console.log('Result: ', result); //return object success
 
-                            fs.unlink(pathFile, (err) => {
-                                if (err) {
-                                    console.error(err)
-                                    return
-                                }
-                                //file removed
-                            });
+                            // fs.unlink(pathFile, (err) => {
+                            //     if (err) {
+                            //         console.error(err)
+                            //         return
+                            //     }
+                            //     //file removed
+                            // });
                         })
                         .catch((erro) => {
                           console.error('Error when sending: ', erro); //return object error
                           
-                          fs.unlink(pathFile, (err) => {
-                                if (err) {
-                                    console.error(err)
-                                    return
-                                }
-                                //file removed
-                            });
+                        //   fs.unlink(pathFile, (err) => {
+                        //         if (err) {
+                        //             console.error(err)
+                        //             return
+                        //         }
+                        //         //file removed
+                        //     });
                         });
 
                     } else if (message.body == '!ping') {
